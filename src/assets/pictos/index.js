683f4e8bd7a9d40cd88ea0dfab33e6f2d035095a
Sébastien Quirrion <sebastien@fitlab.fr>
2022-01-13 17:59:46 +0100

    Ajout partie themes

---
+++ src/assets/pictos/index.js
@@ -0,0 +1,13 @@
+import pictoEchanger from './logo-echanger-proches-min.svg';
+import pictoContenuNum from './logo-contenus-numeriques-min.svg';
+import pictoDemarcheLigne from './logo-demarche-en-ligne-n-min.svg';
+import pictoTrouverEmploi from './logo-trouver-emploi-min.svg';
+import pictoAccompagnerEnfant from './logo-accompagner-enfant-min.svg';
+
+export default {
+  pictoEchanger,
+  pictoContenuNum,
+  pictoDemarcheLigne,
+  pictoTrouverEmploi,
+  pictoAccompagnerEnfant
+};
