ea9d0338755949bfa776637435463bb527c0b613
Sébastien Quirrion <sebastien@fitlab.fr>
2022-01-20 16:32:21 +0100

    Recuperation des 2 stats metabase

---
+++ src/services/statistiques.service.js
@@ -0,0 +1,26 @@
+export const statistiquesService = {
+  getHomeStats
+};
+
+const urlAPI = process.env.REACT_APP_API_URL;
+
+function getHomeStats() {
+  const requestOptions = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
+  };
+
+  return fetch(`${urlAPI}/stats/metabase`, requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+  return response.text().then(text => {
+    const data = text && JSON.parse(text);
+    if (!response.ok) {
+      const error = (data && data.message) || response.statusText;
+      return Promise.reject(error);
+    }
+
+    return data;
+  });
+}
