6f073bfefccd5d6c332d1195f259195b2db41b95
Sébastien Quirrion <sebastien@fitlab.fr>
2022-11-14 22:45:51 +0100

    Ajout scroll

--- src/views/Carte.js
+++ src/views/Carte.js
@@ -5,6 +5,7 @@ const urlPermanences = process.env.REACT_APP_API_URL + '/permanences';
 
 class Carte extends Component {
   render() {
+    window.scrollTo({ top: 0 });
     require('@gouvfr-anct/cartographie-nationale/cartographie');
     return (
       <div className="carte vh-100">
