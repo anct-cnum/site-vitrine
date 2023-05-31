7da69b70726ab93bf88601a475c45f21e874c3ef
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-05-31 15:49:14 +0200

    fix: supp div inutile

--- src/views/coordination-territoriale/Informations.js
+++ src/views/coordination-territoriale/Informations.js
@@ -5,18 +5,14 @@ function Informations() {
   return (
     <section className="section-informations">
       <div className="fr-container fr-pt-10w fr-pb-8w">
-        <div className="fr-grid-row fr-grid-row--gutter">
-          <div className="fr-col-xl-7 fr-col-12">
-            <p style={{ color: 'white', width: '98%' }}>
-              <strong>Le nombre de Conseillers num&eacute;riques coordinateurs&nbsp;</strong>pr&eacute;sents sur le territoire sera
-              <strong>&nbsp;accru&nbsp;</strong>au cours du second trimestre 2023.
-            </p>
-            <p style={{ color: 'white' }}>
-              La <strong>cartographie</strong> consultable ci-dessous permet de localiser es Conseillers
-              num&eacute;riques coordinateurs et d&rsquo;identifier leurs p&eacute;rim&egrave;tres de comp&eacute;tence.
-            </p>
-          </div>
-        </div>
+        <p className="block-text-informations-1">
+          <strong>Le nombre de Conseillers num&eacute;riques coordinateurs&nbsp;</strong>pr&eacute;sents sur le territoire sera
+          <strong>&nbsp;accru&nbsp;</strong>au cours du second trimestre 2023.
+        </p>
+        <p className="block-text-informations-2">
+          La <strong>cartographie</strong> consultable ci-dessous permet de localiser les Conseillers
+          num&eacute;riques coordinateurs et d&rsquo;identifier leurs p&eacute;rim&egrave;tres de comp&eacute;tence.
+        </p>
       </div>
     </section>
   );
