413efe72ca300e06988da59c00447a1da0de0645
Morgan Prioux <68907081+MorganP-projects@users.noreply.github.com>
2024-02-26 16:17:52 +0100

    Update src/views/formation/Question.js
    Co-authored-by: Sébastien Quirrion <77339872+squirrion@users.noreply.github.com>

--- src/views/formation/Question.js
+++ src/views/formation/Question.js
@@ -4,7 +4,7 @@ function Question() {
   const aideUrl = `${process.env.REACT_APP_AIDE_URL}/category/la-formation-des-conseillers-numeriques-objectifs-et-modalite-dorganisation-1dj9ogr/`;
   return (
     <div className="fr-col-9 fr-mt-5w cadre-bleu">
-      <b>Pour toute question complémentaire, consultez la&nbsp;</b>
+      <b>Pour toute question compl&eacute;mentaire, consultez la&nbsp;</b>
       <a href={aideUrl} rel="noopener noreferrer" target="_blank">FAQ Formation</a>
     </div>
     
