d3b275782ec4edffd80f791aed75101431cf9eb1
morgan <morgan@fitlab.fr>
2024-02-27 11:39:08 +0100

    indentation

--- src/views/formation/Introduction.js
+++ src/views/formation/Introduction.js
@@ -8,12 +8,12 @@ function Introduction() {
         <h1 className="fr-h1 fr-mt-10w fr-mb-5w">Formation des conseillers num&eacute;riques</h1>
       </div>
       <div className="fr-col-offset-2 fr-col-10 introduction">
-          En 2024, la formation des conseiller num&eacute;riques reprend et s&rsquo;enrichit&nbsp;!
+        En 2024, la formation des conseiller num&eacute;riques reprend et s&rsquo;enrichit&nbsp;!
       </div>
       <div className="fr-col-offset-2 fr-col-8 introduction">
-          Obligatoire et prise en charge par l&rsquo;&Eacute;tat dans le cadre du dispositif, cette formation<br/>
-          vous permet de d&eacute;velopper vos comp&eacute;tences &agrave; chaque &eacute;tape de votre parcours
-          professionnel en tant que conseiller num&eacute;rique.
+        Obligatoire et prise en charge par l&rsquo;&Eacute;tat dans le cadre du dispositif, cette formation<br/>
+        vous permet de d&eacute;velopper vos comp&eacute;tences &agrave; chaque &eacute;tape de votre parcours
+        professionnel en tant que conseiller num&eacute;rique.
       </div>
     </>
   );
--- src/views/formation/ParcoursFormation.js
+++ src/views/formation/ParcoursFormation.js
@@ -13,10 +13,10 @@ function ParcoursFormation() {
           alt="Parcours de formation depuis la prise de poste, avec une phase de formation initiale, puis une phase de formation continue"/>
       </div>
       <div className="fr-col-offset-2 fr-col-8 description">
-          Cette frise pr&eacute;sente le parcours de formation classique du conseiller num&eacute;rique.
-          L&rsquo;ordre des &eacute;l&eacute;ments, notamment le passage du CCP1, peut &ecirc;tre modifi&eacute;
-          selon les sp&eacute;cificit&eacute;s individuelles et territoriales. En outre-mer, le parcours de
-          formation et le passage du CCP1 sont d&eacute;ploy&eacute;s par POP School.
+        Cette frise pr&eacute;sente le parcours de formation classique du conseiller num&eacute;rique.
+        L&rsquo;ordre des &eacute;l&eacute;ments, notamment le passage du CCP1, peut &ecirc;tre modifi&eacute;
+        selon les sp&eacute;cificit&eacute;s individuelles et territoriales. En outre-mer, le parcours de
+        formation et le passage du CCP1 sont d&eacute;ploy&eacute;s par POP School.
       </div>
       <div className="fr-col-offset-2 fr-col-8 description">
         <b>La Fabrik</b> repr&eacute;sente le consortium La Fabrik de la m&eacute;diation num&eacute;rique port&eacute;par CEFIAC et M2i.<br/>
