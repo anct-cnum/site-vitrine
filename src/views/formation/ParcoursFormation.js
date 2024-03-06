361f07fa0eeb58abdc904e116bcb69c59b2d1a2f
morgan <morgan@fitlab.fr>
2024-03-06 11:57:32 +0100

    déplacement des classes

--- src/views/formation/ParcoursFormation.js
+++ src/views/formation/ParcoursFormation.js
@@ -12,16 +12,16 @@ function ParcoursFormation() {
         <img src={ImageParcours} className="fresque" loading="lazy"
           alt="Parcours de formation depuis la prise de poste, avec une phase de formation initiale, puis une phase de formation continue"/>
       </div>
-      <div className="fr-col-offset-2 fr-col-8 description">
-        <p className="fr-ml-n1w">
+      <div className="fr-col-offset-2 fr-col-8">
+        <p className="fr-ml-n1w description">
           Cette frise pr&eacute;sente le parcours de formation classique du conseiller num&eacute;rique.
           L&rsquo;ordre des &eacute;l&eacute;ments, notamment le passage du CCP1, peut &ecirc;tre modifi&eacute;
           selon les sp&eacute;cificit&eacute;s individuelles et territoriales. En outre-mer, le parcours de
           formation et le passage du CCP1 sont d&eacute;ploy&eacute;s par POP School.
         </p>
       </div>
-      <div className="fr-col-offset-2 fr-col-8 description">
-        <p className="fr-ml-n1w">
+      <div className="fr-col-offset-2 fr-col-8">
+        <p className="fr-ml-n1w description">
           <b>La Fabrik</b> repr&eacute;sente le consortium La Fabrik de la m&eacute;diation num&eacute;rique port&eacute;par CEFIAC et M2i.<br/>
           <b>La Mednum</b> repr&eacute;sente le consortium port&eacute; par la Mednum, les hubs territoriaux pour un num&eacute;rique inclusif et
           organismes de formation impliqu&eacute;s dans la d&eacute;livrance de ces formations.
