8d1b60184a1fc156b2261373ec22717f8a7d848d
morgan <morgan@fitlab.fr>
2024-03-25 14:57:22 +0100

    retour wording

--- src/views/formation/FormationInitiale.js
+++ src/views/formation/FormationInitiale.js
@@ -19,18 +19,34 @@ function FormationInitiale() {
         </p>
       </div>
       <div className="fr-ml-5w fr-col-11">
-        <h2 className="fr-mt-6w fr-mb-3w">Passage CCP1</h2>
+        <h2 className="fr-mt-6w fr-mb-3w">Passage du CCP1</h2>
       </div>
       <div className="fr-ml-5w fr-col-11">
         <p>
           Le passage du CCP1 du titre professionnel de Responsable d&rsquo;espace de m&eacute;diation num&eacute;rique, qui atteste la
-          capacit&eacute; &agrave; accompagner des publics divers vers l&rsquo;autonomie num&eacute;rique, est le point final de votre
-          formation initiale. Pour vous y pr&eacute;parer, un parcours de formation en 70, 175 ou 315h vous est propos&eacute; selon
-          votre profil et votre exp&eacute;rience.
+          capacit&eacute; &agrave; accompagner des publics divers vers l&rsquo;autonomie num&eacute;rique, est le point final de votre formation initiale.
         </p>
         <p>
-          Les inscriptions ont lieu directement aupr&egrave;s de votre organisme de formation, qui vous recontactera pour d&eacute;finir
-          avec vous le parcours le plus adapt&eacute; et vous proposer un calendrier de formation.
+          Pour vous y pr&eacute;parer, trois parcours de formation vous sont propos&eacute;s : 70h (court), 175h (interm&eacute;diaire)
+          ou 315h (long). Un test de positionnement, en deux temps, sera organis&eacute; pour identifier le parcours le plus ad&eacute;quat
+          &agrave; vos besoins :
+          <ul>
+            <li>
+              Un test en ligne et en autonomie (d&rsquo;1h30 maximum) permettra une premi&egrave;re &eacute;valuation de votre culture
+              citoyenne et num&eacute;rique, et de vos comp&eacute;tences informatiques et p&eacute;dagogiques
+            </li>
+            <li>
+              Vous passerez ensuite un entretien avec une &eacute;quipe de l&rsquo;organisme de formation, o&ugrave; vous aurez
+              l&rsquo;occasion de discuter ensemble des r&eacute;sultats du test en ligne, et de revenir sur votre exp&eacute;rience
+              professionnelle et vos motivations. A la fin de l&rsquo;entretien, un petit temps d&rsquo;&eacute;change pourra &ecirc;tre
+              pr&eacute;vu avec votre structure d&rsquo;accueil pour pr&eacute;senter le d&eacute;roul&eacute; de la formation, en fonction
+              du parcours choisi.
+            </li>
+          </ul>
+        </p>
+        <p>
+          Les inscriptions ont lieu directement aupr&egrave;s de votre organisme de formation, qui vous contactera pour d&eacute;finir avec
+          vous le parcours le plus adapt&eacute; et vous proposer un calendrier de formation.
         </p>
         <p>
           <b>Inscriptions M&eacute;tropole&nbsp;:&nbsp;</b>
@@ -61,7 +77,9 @@ function FormationInitiale() {
               </p>
             </div>
             <div className="fr-col-xs-12 fr-col-xl-5 fr-m-3w img-fabrik">
-              <img src={ImageLaFabrik} alt="image pr&eacute;sentant le site de La Fabrik" loading="lazy"/>
+              <a href={fabrikUrl} rel="noopener noreferrer" target="_blank" className="lien-image-fabrik">
+                <img src={ImageLaFabrik} alt="image pr&eacute;sentant le site de La Fabrik" loading="lazy"/>
+              </a>
             </div>
           </div>
         </div>
