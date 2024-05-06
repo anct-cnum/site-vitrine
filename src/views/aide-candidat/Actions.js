1dd3fca20f7db35556d347c1b540f084993eb409
dienamo <46058257+dienamo@users.noreply.github.com>
2024-05-06 18:41:15 +0200

    :adhesive_bandage: création page formulaire candidat indisponible wip

--- src/views/aide-candidat/Actions.js
+++ src/views/aide-candidat/Actions.js
@@ -7,7 +7,7 @@ function Actions() {
   return (
     <div className="fr-container fr-col-lg-5 fr-col-xs-12 fr-mb-10w" style={{ textAlign: 'center' }}>
       <a
-        href={`${urlFormConseiller}/formulaire-indisponible`}
+        href={`${urlFormConseiller}`}
         target="_blank" rel="noopener noreferrer"
         className="buttonCustom blueButtonCustom fr-text--xx-bold"
         title="Devenir conseiller num&eacute;rique">Devenir conseiller num&eacute;rique</a>
