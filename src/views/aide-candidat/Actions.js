03cc4022fa164bfa0c201254011eb6c1ee88b6b4
dienamo <46058257+dienamo@users.noreply.github.com>
2024-05-06 17:29:52 +0200

    :adhesive_bandage: création page formulaire candidat indisponible

--- src/views/aide-candidat/Actions.js
+++ src/views/aide-candidat/Actions.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Actions() {
 
-  const urlFormConseiller = process.env.REACT_APP_FORMS_URL + '/conseiller/new';
+  const urlFormConseiller = process.env.REACT_APP_FORMS_URL + '/formulaire-indisponible';
 
   return (
     <div className="fr-container fr-col-lg-5 fr-col-xs-12 fr-mb-10w" style={{ textAlign: 'center' }}>
-      <a
-        href={urlFormConseiller}
+      <Link
+        to={`${urlFormConseiller}/formulaire-indisponible`}
         target="_blank" rel="noopener noreferrer"
         className="buttonCustom blueButtonCustom fr-text--xx-bold"
-        title="Devenir conseiller num&eacute;rique">Devenir conseiller num&eacute;rique</a>
+        title="Devenir conseiller num&eacute;rique">Devenir conseiller num&eacute;rique</Link>
     </div>
   );
 }
