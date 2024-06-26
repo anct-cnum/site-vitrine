d5993815f13e048d290986e635bc95062077b85b
Fabien Mercier <fabien.mercier@octo.com>
2024-06-26 14:01:26 +0200

    Mise en place du formulaire candidature d'un conseiller

---
+++ src/views/candidature-conseiller/CandidatureConseiller.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Sommaire from './Sommaire';
+import InformationsDeContact from './InformationsDeContact';
+import SituationEtExperience from './SituationEtExperience';
+import './CandidatureConseiller.css';
+
+export default function CandidatureConseiller() {
+  return (
+    <div className="fr-container fr-mt-5w fr-mb-5w">
+      <div className="fr-grid-row">
+        <div className="fr-col-4">
+          <Sommaire />
+        </div>
+        <div className="fr-col-8">
+          <h1 className="cc-titre fr-mb-5w">Je veux devenir conseiller numérique</h1>
+          <form aria-label="Candidature conseiller">
+            <InformationsDeContact />
+            <SituationEtExperience />
+            <button className="fr-btn" type="submit">
+              Envoyer votre candidature
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
