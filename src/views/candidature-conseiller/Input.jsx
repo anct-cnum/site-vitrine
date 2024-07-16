2e9cad494d18b520353d7818f22bad71e5abe3e1
Alezco <benjaminmorali4@hotmail.fr>
2024-07-16 17:47:03 +0200

    Ajout de la vérification de numéro de téléphone

--- src/views/candidature-conseiller/Input.jsx
+++ src/views/candidature-conseiller/Input.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Input({ children, id, isRequired = true, type = 'text' }) {
+export default function Input({ children, id, isRequired = true, type = 'text', pattern }) {
   return (
     <div className="fr-fieldset__element">
       <div className="fr-input-group">
         <label className="fr-label" htmlFor={id}>{children}</label>
-        <input className="fr-input" type={type} id={id} required={isRequired} />
+        <input className="fr-input" type={type} id={id} required={isRequired} pattern={pattern} />
       </div>
     </div>
   );
@@ -17,4 +17,5 @@ Input.propTypes = {
   id: PropTypes.string,
   isRequired: PropTypes.bool,
   type: PropTypes.string,
+  pattern: PropTypes.string,
 };
--- src/views/candidature-conseiller/SituationEtExperience.jsx
+++ src/views/candidature-conseiller/SituationEtExperience.jsx
@@ -3,6 +3,25 @@ import Checkbox from './Checkbox';
 import BoutonRadio from './BoutonRadio';
 
 export default function SituationEtExperience() {
+  const situations = [
+    {
+      id: 'demandeurEmploi',
+      libelle: 'Demandeur d’emploi'
+    },
+    {
+      id: 'enEmploi',
+      libelle: 'En emploi'
+    },
+    {
+      id: 'enFormation',
+      libelle: 'En formation'
+    },
+    {
+      id: 'diplome',
+      libelle: 'Diplômé dans le secteur de la médiation numérique (formation certifiante ou non)'
+    }
+  ];
+
   return (
     <fieldset className="fr-border cc-section fr-p-3w fr-mb-3w" id="situationEtExperience">
       <legend className="fr-h5">Votre situation et expérience</legend>
@@ -10,18 +29,11 @@ export default function SituationEtExperience() {
       <p className="fr-mb-3w cc-bold">
         Êtes-vous actuellement dans l’une des situations suivantes ? <span className="cc-obligatoire">*</span>
       </p>
-      <Checkbox id="demandeurEmploi">
-        Demandeur d’emploi
-      </Checkbox>
-      <Checkbox id="enEmploi">
-        En emploi
-      </Checkbox>
-      <Checkbox id="enFormation">
-        En formation
-      </Checkbox>
-      <Checkbox id="diplome">
-        Diplômé dans le secteur de la médiation numérique (formation certifiante ou non)
-      </Checkbox>
+      {situations.map(({ id, libelle }) =>
+        <Checkbox id={id} key={id}>
+          {libelle}
+        </Checkbox>
+      )}
       <hr />
       <p className="fr-mb-3w cc-bold">
         Avez-vous une expérience professionnelle de médiation numérique ? <span className="cc-obligatoire">*</span>
