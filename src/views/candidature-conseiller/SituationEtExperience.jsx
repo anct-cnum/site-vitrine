924bb81895bd97fd74c538bca658eeb8a3fb490c
Alezco <benjaminmorali4@hotmail.fr>
2024-07-17 15:51:51 +0200

    Ajout d'un message d'erreur sur la validation des checkboxes

--- src/views/candidature-conseiller/SituationEtExperience.jsx
+++ src/views/candidature-conseiller/SituationEtExperience.jsx
@@ -4,26 +4,40 @@ import BoutonRadio from './BoutonRadio';
 import Input from './Input';
 
 export default function SituationEtExperience() {
-  const [isDiplomeSelected, setIsDiplomeSelected] = useState(false);
-
   const situations = [
     {
       id: 'demandeurEmploi',
-      libelle: 'Demandeur d’emploi'
+      libelle: 'Demandeur d’emploi',
     },
     {
       id: 'enEmploi',
-      libelle: 'En emploi'
+      libelle: 'En emploi',
     },
     {
       id: 'enFormation',
-      libelle: 'En formation'
+      libelle: 'En formation',
     },
     {
       id: 'diplome',
-      libelle: 'Diplômé dans le secteur de la médiation numérique (formation certifiante ou non)'
+      libelle: 'Diplômé dans le secteur de la médiation numérique (formation certifiante ou non)',
     }
   ];
+
+  const [isDiplomeSelected, setIsDiplomeSelected] = useState(false);
+  const [isSituationValid, setIsSituationValid] = useState(true);
+  const [checkedState, setCheckedState] = useState(
+    new Array(situations.length).fill(false)
+  );
+
+  const handleCheck = event => {
+    const indexCaseCochee = situations.findIndex(situation => situation.id === event.target.id);
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === indexCaseCochee ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+    setIsSituationValid(updatedCheckedState.some(checked => checked));
+    setIsDiplomeSelected(event.target.id === 'diplome' && event.target.checked);
+  };
 
   return (
     <fieldset className="fr-border cc-section fr-p-3w fr-mb-3w" id="situationEtExperience">
@@ -32,12 +46,16 @@ export default function SituationEtExperience() {
       <p className="fr-mb-3w cc-bold">
         Êtes-vous actuellement dans l’une des situations suivantes ? <span className="cc-obligatoire">*</span>
       </p>
-      {situations.map(({ id, libelle }) =>
-        <Checkbox id={id} key={id} onCheck={event => setIsDiplomeSelected(event.target.id === 'diplome' && event.target.checked)}>
+      {situations.map(({ id, libelle }, index) =>
+        <Checkbox id={id} key={id} onCheck={handleCheck} checked={checkedState[index]}>
           {libelle}
         </Checkbox>
       )}
-      {isDiplomeSelected &&
+      {!isSituationValid && <div className="fr-messages-group fr-pl-1w" id="checkboxes-error-messages" aria-live="assertive">
+        <p className="fr-message fr-message--error" id="checkboxes-error-message-error">Vous devez cocher au moins une case</p>
+      </div>}
+      {
+        isDiplomeSelected &&
         <Input
           id="detailDiplome"
           isRequired={false}
@@ -56,6 +74,6 @@ export default function SituationEtExperience() {
       <BoutonRadio id="non" nomGroupe="experienceProfessionnelle">
         Non
       </BoutonRadio>
-    </fieldset>
+    </fieldset >
   );
 }
