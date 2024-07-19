7612895eee494f177ff9104dadd4397ce0453a1f
Fabien Mercier <fabien.mercier@octo.com>
2024-07-19 10:37:23 +0200

    Changement de lien sur l'accueil

--- src/views/accueil/Aide.js
+++ src/views/accueil/Aide.js
@@ -1,31 +1,32 @@
 import React from 'react';
+
 import logoFS from '../../assets/brands/logo-france-services-min.svg';
 
 function Aide() {
-
   return (
     <div className="aide fr-pt-7w fr-pb-9w">
-      <div className="rowCustom" >
+      <div className="rowCustom">
         <div className="partLogo">
-          <img src={logoFS} alt="Logo France Services" className="logoFS" />
+          <img src={logoFS} alt="" className="logoFS" />
         </div>
         <div className="partText">
           <h1 className="fr-h1 title fr-mb-4w">
-            J&rsquo;ai besoin d&rsquo;aide pour mes d&eacute;marches administratives
+            J’ai besoin d’aide pour mes démarches administratives
           </h1>
           <p>
-            &Agrave; moins de 30 minutes de chez vous, les agents France services vous accompagnent pour r&eacute;aliser vos d&eacute;marches aupr&egrave;s
-            de 9 op&eacute;rateurs nationaux&nbsp;: les minist&egrave;res de l&rsquo;Int&eacute;rieur et de la Justice, les services des imp&ocirc;ts (DGFIP),
-            P&ocirc;le Emploi, la Caisse d&rsquo;allocations familiales (CAF), l&rsquo;Assurance maladie (CNAM), l&rsquo;Assurance Vieillesse (CNAV),
-            la Mutualit&eacute; sociale agricole (MSA) et La Poste.
+            À moins de 30 minutes de chez vous, les agents France services vous accompagnent pour réaliser vos démarches auprès
+            de 9 opérateurs nationaux : les ministères de l’Intérieur et de la Justice, les services des impôts (DGFIP),
+            Pôle Emploi, la Caisse d’allocations familiales (CAF), l’Assurance maladie (CNAM), l’Assurance Vieillesse (CNAV),
+            la Mutualité sociale agricole (MSA) et La Poste.
             <br />
             <a
               className="fr-link linkCustom"
-              title="En savoir plus"
-              href="https://www.cohesion-territoires.gouv.fr/france-services"
+              title="Trouver la France services la plus proche de chez vous"
+              href="https://www.france-services.gouv.fr/"
               target="_blank"
-              rel="noopener noreferrer">
-                En savoir plus
+              rel="noopener noreferrer"
+            >
+              En savoir plus
             </a>
           </p>
         </div>
