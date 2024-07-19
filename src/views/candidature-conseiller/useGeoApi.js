014f249dc74bd96cfa9f614dbc2a46c33df76910
Alezco <benjaminmorali4@hotmail.fr>
2024-07-19 16:14:16 +0200

    Retours de code review

--- src/views/candidature-conseiller/useGeoApi.js
+++ src/views/candidature-conseiller/useGeoApi.js
@@ -1,10 +1,16 @@
+import { useState } from 'react';
+
 export const useGeoApi = () => {
   const urlBase = 'https://geo.api.gouv.fr/communes?limit=10&fields=nom,code&';
-  const fetchVilles = async rechercheUtilisateur => {
+
+  const [villes, setVilles] = useState([]);
+
+  const search = async rechercheUtilisateur => {
     const url = `${urlBase}&nom=${rechercheUtilisateur}`;
     const villes = await fetch(url);
-    return await villes.json();
+    const resultat = await villes.json();
+    setVilles(resultat);
   };
 
-  return { fetchVilles };
+  return { search, villes };
 };
