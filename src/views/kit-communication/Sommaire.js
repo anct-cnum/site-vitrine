f7241bf647698c737d0d92bbb49ea939db738569
Fabien Mercier <fabien.mercier@octo.com>
2024-07-25 09:28:18 +0200

    Modification du wording à propos du téléchargement du kit de communication

--- src/views/kit-communication/Sommaire.js
+++ src/views/kit-communication/Sommaire.js
@@ -1,105 +1,109 @@
 import React from 'react';
 
 function Sommaire() {
-
   return (
-    <div className="fr-grid-row fr-grid-row--gutter">
-      <div className="fr-col-1"></div>
-      <div className="fr-col-10 fr-col-md-5 fr-mb-9w">
-        <nav className="fr-summary summary-custom" role="navigation" aria-labelledby="fr-summary-title">
+    <div className="fr-grid-row fr-grid-row--gutter fr-grid-row--center">
+      <div className="fr-col-8 fr-col-md-5 fr-mb-9w">
+        <nav className="fr-summary summary-custom" aria-labelledby="fr-summary-title">
           <div className="summary-title-custom fr-mb-6w" id="fr-summary-title">Sommaire</div>
           <ol className="summary-list-custom">
             <li>
               <a className="summary-link-custom summary-link-custom-bold" href="#titre-ancre-1">
-                1.&nbsp;&nbsp;Pr&eacute;sentation du dispositif Conseiller num&eacute;rique
+                1.&nbsp;Présentation du dispositif Conseiller numérique
               </a>
             </li>
             <li>
               <a className="summary-link-custom summary-link-custom-bold" href="#titre-ancre-2">
-                2.&nbsp;&nbsp;Charte institutionnelle et identit&eacute; visuelle
+                2.&nbsp;Charte institutionnelle et identité visuelle
               </a>
             </li>
             <li>
-              <a className="summary-link-custom" href="#titre-ancre-2.1">2.1&nbsp;&nbsp;Orthographe</a>
+              <a className="summary-link-custom" href="#titre-ancre-2.1">2.1&nbsp;Orthographe</a>
             </li>
             <li>
-              <a className="summary-link-custom" href="#titre-ancre-2.2">2.2&nbsp;&nbsp;Charte Graphique</a>
+              <a className="summary-link-custom" href="#titre-ancre-2.2">2.2&nbsp;Charte Graphique</a>
             </li>
             <li>
-              <a className="summary-link-custom" href="#titre-ancre-2.3">2.3&nbsp;&nbsp;Logotypes</a>
+              <a className="summary-link-custom" href="#titre-ancre-2.3">2.3&nbsp;Logotypes</a>
             </li>
             <li>
-              <a className="summary-link-custom" href="#titre-ancre-2.4">2.4&nbsp;&nbsp;Illustrations</a>
+              <a className="summary-link-custom" href="#titre-ancre-2.4">2.4&nbsp;Illustrations</a>
             </li>
             <li>
-              <a className="summary-link-custom summary-link-custom-bold" href="#titre-ancre-3">3.&nbsp;&nbsp;Supports de communication</a>
+              <a className="summary-link-custom summary-link-custom-bold" href="#titre-ancre-3">3.&nbsp;Supports de communication</a>
             </li>
             <li>
-              <a className="summary-link-custom" href="#titre-ancre-3.1">3.1&nbsp;&nbsp;Plaquette de pr&eacute;sentation du dispositif Conum</a>
+              <a className="summary-link-custom" href="#titre-ancre-3.1">3.1&nbsp;Plaquette de présentation du dispositif Conum</a>
             </li>
             <li>
               <a className="summary-link-custom" href="#titre-ancre-3.2">
-                3.2&nbsp;&nbsp;Affiche &laquo;&nbsp;Trouver un conseiller num&eacute;rique pr&egrave;s de chez soi&nbsp;&raquo;
+                3.2&nbsp;Affiche &laquo;&nbsp;Trouver un conseiller numérique près de chez soi&nbsp;&raquo;
               </a>
             </li>
             <li>
-              <a className="summary-link-custom" href="#titre-ancre-3.3">3.3&nbsp;&nbsp;Gabarits pour les r&eacute;seaux sociaux</a>
+              <a className="summary-link-custom" href="#titre-ancre-3.3">3.3&nbsp;Gabarits pour les réseaux sociaux</a>
             </li>
             <li>
-              <a className="summary-link-custom" href="#titre-ancre-3.4">3.4&nbsp;&nbsp;Mod&egrave;le de pr&eacute;sentation Powerpoint</a>
+              <a className="summary-link-custom" href="#titre-ancre-3.4">3.4&nbsp;Modèle de présentation Powerpoint</a>
             </li>
             <li>
-              <a className="summary-link-custom" href="#titre-ancre-3.5">3.5&nbsp;&nbsp;Affiche A3 (gabarit)</a>
+              <a className="summary-link-custom" href="#titre-ancre-3.5">3.5&nbsp;Affiche A3 (gabarit)</a>
             </li>
             <li>
               <a className="summary-link-custom summary-link-custom-bold" href="#titre-ancre-4">
-                4.&nbsp;&nbsp;Outils &agrave; destination des conseillers num&eacute;riques
+                4.&nbsp;Outils à destination des conseillers numériques
               </a>
             </li>
             <li>
-              <a className="summary-link-custom" href="#titre-ancre-4.1">4.1&nbsp;&nbsp;Cartes de visite imprimables</a>
+              <a className="summary-link-custom" href="#titre-ancre-4.1">4.1&nbsp;Cartes de visite imprimables</a>
             </li>
             <li>
-              <a className="summary-link-custom" href="#titre-ancre-4.2">4.2&nbsp;&nbsp;Signature mail</a>
+              <a className="summary-link-custom" href="#titre-ancre-4.2">4.2&nbsp;Signature e-mail</a>
             </li>
             <li>
-              <a className="summary-link-custom" href="#titre-ancre-4.3">4.3&nbsp;&Eacute;tiquettes</a>
+              <a className="summary-link-custom" href="#titre-ancre-4.3">4.3&nbsp;étiquettes</a>
             </li>
           </ol>
         </nav>
       </div>
-      <div className="fr-col-10 fr-col-md-5 fr-mb-9w about-part">
-        <div className="about-title-custom fr-mb-6w">&Agrave; propos</div>
-        <p style={{ marginTop: '-0.4rem' }}>
-          Vous d&eacute;ployez des actions dans le cadre de Conseiller num&eacute;rique ?
-          Un kit a &eacute;t&eacute; sp&eacute;cialement conçu pour valoriser le dispositif sur vos diff&eacute;rents canaux de communication.
-          <br/><br/>
-          Des supports sont &eacute;galement adress&eacute;s sp&eacute;cifiquement aux conseillers num&eacute;riques :
-          signatures mail, cartes de visite et &eacute;tiquettes.
-          <br/><br/><br/>
-          <em><span style={{ fontWeight: 700, fontSize: '12px' }}>
-            Kit de communication version 4</span><span className="dateKit">&nbsp;– f&eacute;vrier 2024
-          </span></em>
-          <br/><br/>
-          <span style={{ fontSize: '12px' }}><em>Liste des nouveaut&eacute;s&nbsp;:<br/>
-          – mise &agrave; jour du logo Conseiller num&eacute;rique, fichiers sources et supports&nbsp;;<br/>
-          – ajout du logo en version f&eacute;minine&nbsp;;<br/>
-          – ajout de l&rsquo;affiche &laquo;&nbsp;Trouver un Conseiller num&eacute;rique pr&egrave;s de chez soi&nbsp;&raquo;.<br/>
-          – ajout de nouvelles illustrations&nbsp;;<br/>
-          – mise &agrave; jour des bonnes pratiques r&eacute;dactionnelles et de la charte graphique&nbsp;;<br/>
-          – retrait du bloc-marque France Relance</em></span>
+      <div className="fr-col-8 fr-col-md-5 fr-mb-9w sommaire">
+        <div className="about-title-custom fr-mb-6w">À propos</div>
+        <p>
+          Vous déployez des actions dans le cadre de Conseiller numérique ?
+          Un kit a été spécialement conçu pour valoriser le dispositif sur vos différents canaux de communication.
         </p>
-        <div className="about-div">
-          <p className="about-p">
-            <a href="https://cdn.conseiller-numerique.gouv.fr/kit-communication/V4/kit-conum-v4-complet.zip"
-              title="archive compl&egrave;te du kit"
-              className="bleu-france"
-              style={{ boxShadow: 'none' }}
-              target="_blank"
-              rel="noopener noreferrer">
-              T&eacute;l&eacute;charger la version compl&egrave;te du kit de communication Conseiller num&eacute;rique (85,1Mo).
-            </a>
+        <p>
+          Des supports sont également adressés spécifiquement aux conseillers numériques : signatures e-mail, cartes de visite et étiquettes.
+        </p>
+        <div className="cadre-kit">
+          <p className="bleu-france">
+            <span className="fr-icon-flashlight-fill flashlight-icon" aria-hidden="true"></span>&nbsp;
+            Kit de communication version 4 – février 2024
           </p>
+          <p>
+            La charte graphique du dispositif Conseiller numérique a évolué.
+            Sa nouvelle version propose une charte qui ne comprend plus le logo « France Relance »,
+            dispositif européen dont étaient originaires les fonds ayant permis de financer
+            les deux premières années de chaque poste attribué. Ainsi, il convient d’utiliser :
+          </p>
+          <ul>
+            <li>
+              la charte comportant le logo France Relance lorsque les postes de conseillers numériques
+              sur lesquels vous communiquez sont toujours financés dans le cadre des fonds France Relance,
+            </li>
+            <li>
+              la nouvelle charte (sans logo France Relance), si vos postes de conseillers numériques
+              ont été renouvelés et sont donc dans leur 3e, 4e ou 5e année de financement.
+            </li>
+          </ul>
+          <a
+            href="https://cdn.conseiller-numerique.gouv.fr/kit-communication/V4/kit-conum-v4-complet.zip"
+            className="fr-btn fr-btn--secondary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Télécharger le kit complet (85,1 Mo)
+          </a>
         </div>
       </div>
     </div>
