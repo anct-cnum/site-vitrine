3b74bef31595755d575207662fc13c69d9db09d8
dienamo <dienamoustapha@gmail.com>
2024-09-11 16:17:52 +0100

    :feat refactorisation du hook de gestion des données insee ou adresse

--- src/views/candidature-structure/useEntrepriseFinder.js
+++ src/views/candidature-structure/useEntrepriseFinder.js
@@ -5,17 +5,42 @@ const TAILLE_SIRET = 14;
 const TAILLE_RIDET = [6, 7];
 const TAILLES_POSSIBLES = [...TAILLE_RIDET, TAILLE_SIRET];
 
-export const useEntrepriseFinder = () => {
+export const useEntrepriseFinder = setGeoLocation => {
   const [entreprise, setEntreprise] = useState(null);
   const [error, setError] = useState(null);
   const [addressSuggestions, setAddressSuggestions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [addressLoading, setAddressLoading] = useState(false);
 
   const isValidSiretOrRidet = value => {
     const numericValue = value.replace(/\D/g, '');
     return TAILLES_POSSIBLES.includes(numericValue.length);
   };
 
+  const clearEntrepriseData = () => {
+    setEntreprise(null);
+    setError(null);
+    setGeoLocation(null);
+    document.getElementById('denomination').value = '';
+    document.getElementById('adresse').value = '';
+  };
+
+  const getGeoLocationFromAddress = async address => {
+    try {
+      const urlAPI = `https://api-adresse.data.gouv.fr/search/?q=${encodeURIComponent(address)}`;
+      const response = await axios.get(urlAPI);
+      if (response.data.features && response.data.features.length > 0) {
+        const geoLocation = response.data.features[0].geometry;
+        return geoLocation;
+      }
+    } catch (error) {
+      setError('Impossible de trouver la localisation de l\'adresse.');
+    }
+    return null;
+  };
+
   const search = async siretOrRidet => {
+    clearEntrepriseData();
     setEntreprise(null);
     setError(null);
     setAddressSuggestions([]);
@@ -23,17 +48,26 @@ export const useEntrepriseFinder = () => {
       setError('Veuillez entrer un RIDET (6 ou 7 chiffres) ou un SIRET (14 chiffres) valide.');
       return;
     }
+    setLoading(true);
     try {
-      const response = await fetch(`${process.env.VITE_APP_API_URL}/structure/verify-siret-or-ridet/${siretOrRidet}`);
+      const response = await fetch(`http://localhost:8080/structure/verify-siret-or-ridet/${siretOrRidet}`);
       if (!response.ok) {
         throw new Error(`Error fetching data: ${response.statusText}`);
       }
       const result = await response.json();
       setEntreprise(result);
+      if (siretOrRidet.length === TAILLE_SIRET && result.adressStructure) {
+        const geoLocation = await getGeoLocationFromAddress(result.adressStructure);
+        if (geoLocation) {
+          setGeoLocation(geoLocation);
+        }
+      }
       setError(null);
     } catch (err) {
       setError(err.message);
       setEntreprise(null);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -42,19 +76,21 @@ export const useEntrepriseFinder = () => {
       setAddressSuggestions([]);
       return;
     }
-
     try {
+      setAddressLoading(true);
       const urlAPI = `https://api-adresse.data.gouv.fr/search/?q=${encodeURIComponent(adressePostale)}`;
       const response = await axios.get(urlAPI);
       const suggestions = response.data.features.map(feature => ({
         label: feature.properties.label,
-        score: feature.properties.score
+        geometry: feature.geometry
       }));
       setAddressSuggestions(suggestions);
     } catch (error) {
       setAddressSuggestions([]);
+    } finally {
+      setAddressLoading(false);
     }
   };
 
-  return { search, entreprise, error, getAddressSuggestions, addressSuggestions };
+  return { search, entreprise, error, getAddressSuggestions, addressSuggestions, loading, addressLoading, clearEntrepriseData };
 };
