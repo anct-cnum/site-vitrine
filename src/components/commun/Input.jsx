78c0a088896f5c6369de2002eec1d11f29ed1cb3
dienamo <dienamoustapha@gmail.com>
2024-09-11 16:08:10 +0100

    :feat ajout du loader dans l'input

--- src/components/commun/Input.jsx
+++ src/components/commun/Input.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Input({ children, id, isRequired = true, type = 'text', pattern, onChange, list, min, readOnly }) {
+export default function Input({ children, id, isRequired = true, type = 'text', pattern, onChange, list, min, readOnly, isLoading, name, value }) {
   return (
     <div className="fr-fieldset__element">
       <div className="fr-input-group">
         <label className="fr-label" htmlFor={id}>{children}</label>
-        <input className="fr-input" type={type} id={id} required={isRequired} pattern={pattern} onChange={onChange} list={list} min={min} readOnly={readOnly}/>
+        <input
+          className="fr-input"
+          type={type}
+          id={id}
+          required={isRequired}
+          pattern={pattern}
+          onChange={onChange}
+          list={list}
+          min={min}
+          readOnly={readOnly}
+          name={name}
+          value={value}
+        />
+        {isLoading && (
+          <div className="fr-input-spinner" aria-hidden="true">
+            <div className="fr-spinner fr-spinner--sm" aria-label="Chargement..."></div>
+          </div>
+        )}
       </div>
     </div>
   );
@@ -21,5 +38,8 @@ Input.propTypes = {
   onChange: PropTypes.func,
   list: PropTypes.string,
   min: PropTypes.string,
-  readOnly: PropTypes.bool
+  readOnly: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  name: PropTypes.string,
+  value: PropTypes.string,
 };
