bd1029affbe065f94c6c353d185456e202b0832a
ornella <ornella93@outlook.fr>
2024-11-27 15:41:44 +0100

    fix boutton sans ouvrir d'onglet

--- src/views/aide-candidat/Actions.js
+++ src/views/aide-candidat/Actions.js
@@ -8,7 +8,7 @@ function Actions() {
     <div className="fr-container fr-col-lg-5 fr-col-xs-12 fr-mb-10w" style={{ textAlign: 'center' }}>
       <a
         href={urlFormConseiller}
-        target="_blank" rel="noopener noreferrer"
+        target="_self" rel="noopener noreferrer"
         className="buttonCustom blueButtonCustom fr-text--xx-bold"
         title="Devenir conseiller num&eacute;rique">Devenir conseiller num&eacute;rique</a>
     </div>
